e109b4fe3fb2c4268f58c83168ba084edf59b9c9
Chiho Sin <chihosin@icloud.com>
2025-07-17 21:54:30 +0800

    Remove obsolete board configurations for various Nordic nRF52 based development kits and update platform metadata to reflect new repository and framework versions.

--- platform.py
+++ platform.py
@@ -21,7 +21,7 @@ from platformio.public import PlatformBase
 
 IS_WINDOWS = sys.platform.startswith("win")
 
-class Nordicnrf52Platform(PlatformBase):
+class Fobenordicnrf52Platform(PlatformBase):
 
     def is_embedded(self):
         return True
